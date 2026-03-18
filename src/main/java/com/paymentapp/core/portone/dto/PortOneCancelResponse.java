b956f5f47143469e7baf62a6c2cd8f6cf2658ec1
sohyun kim <sohyun050282@gmail.com>
2026-03-18 09:43:13 +0900

    feat: 결제 취소 기능 추가

--- src/main/java/com/paymentapp/core/portone/dto/PortOneCancelResponse.java
+++ src/main/java/com/paymentapp/core/portone/dto/PortOneCancelResponse.java
@@ -1,6 +1,7 @@
 package com.paymentapp.core.portone.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -8,6 +9,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class PortOneCancelResponse {
+    @JsonProperty("id")
     private String paymentId;
     private String status;
 }
