b6234f674e97f231f35cb72971dde3ae3b7116cd
sohyun kim <sohyun050282@gmail.com>
2026-03-20 10:32:40 +0900

    fix: 소스 정리 및 리팩토링

--- src/main/java/com/paymentapp/api/webhook/WebhookController.java
+++ src/main/java/com/paymentapp/api/webhook/WebhookController.java
@@ -1,12 +1,9 @@
 package com.paymentapp.api.webhook;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @RestController
